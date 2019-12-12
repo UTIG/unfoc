34a7e344d61512d2a6081189b878688bf046198e
WAIS <wais@freeze.ig.utexas.edu>
2019-12-12 10:41:54 -0600

    Improving coverage and running more test cases

--- regress/run_regress.sh
+++ regress/run_regress.sh
@@ -10,7 +10,12 @@ D0=`dirname $0`
 
 echo "Changing to $D0"
 pushd $D0
+
+# Quick data testing
+#OUTDIR=./covdata_quick
+#UNFOCFLAGS="--nmax 1000"
 OUTDIR=./covdata
+UNFOCFLAGS="--nmax 10000"
 
 RCFILE=`pwd`/.coveragerc
 
@@ -22,8 +27,6 @@ export COVERAGE_PROCESS_START=$RCFILE
 rm -rf $OUTDIR
 
 COVFLAGS="-a"
-# max number of samples to process
-UNFOCFLAGS="--nmax 100"
 
 $COV run ../parse_channels.py
 echo "Done parse_channel"
@@ -45,15 +48,37 @@ HICARS2_CHANNELS="[1,1,1,0,0;2,2,1,0,0]"
 MARFA_CHANNELS="[1,1,1,3,1;5,1,1,0,0;7,3,1,0,0;2,2,1,4,1;6,2,1,0,0;8,4,1,0,0]"
 
 
-echo "${S0}: Start unfoc"
+#echo "${S0}: Checksum input data"
+# TODO: put this into a makefile
+#stdbuf -o 0 sha1sum -b \
+#    $WAIS/orig/xlob/MBL/JKB2h/Y90a/RADnh3/bxds \
+#    $WAIS/orig/xlob/MBL/MKB2l/Y76a/RADnh3/bxds \
+#    $WAIS/orig/xlob/ASB1/GCX0e/R40a/RADnh5/bxds \
+#    $WAIS/orig/xlob/ASB1/JKB2s/R06a/RADnh5/bxds \
+#    | tee $OUTDIR/input_bxds.sha1
+
+
+
+
+echo "${S0}: Running tests"
 ###############
 #test_HiCARS2
-echo $COV run $COVFLAGS ../unfoc.py \
+
+# Run a short section for coverage and no blanking
+$COV run $COVFLAGS ../unfoc.py \
     --infile $WAIS/orig/xlob/MBL/JKB2h/Y90a/RADnh3/bxds \
     --outdir $OUTDIR/MBL/JKB2h/Y90a/RADnh3 \
-    --channel_def ${HICARS2_CHANNELS} --output_samples 3200 --output_phases \
-    --StackDepth 10 --IncoDepth 5  --Scale 20000  --blanking 200 $UNFOCFLAGS
+    --channel_def ${HICARS2_CHANNELS} --output_samples 3200 \
+    --StackDepth 10 --IncoDepth 5  --Scale 20000 --nmax 100
 
+# Run a short section for coverage and no blanking
+$COV run $COVFLAGS ../unfoc.py \
+    --infile $WAIS/orig/xlob/MBL/JKB2h/Y90a/RADnh3/bxds \
+    --outdir $OUTDIR/MBL/JKB2h/Y90a/RADnh3 \
+    --channel_def ${HICARS2_CHANNELS} --output_samples 3200 \
+    --StackDepth 10 --IncoDepth 5  --Scale 20000 --blanking -200 --nmax 100
+
+# Run the same section with blanking from the end
 $COV run $COVFLAGS ../unfoc.py \
     --infile $WAIS/orig/xlob/MBL/JKB2h/Y90a/RADnh3/bxds \
     --outdir $OUTDIR/MBL/JKB2h/Y90a/RADnh3 \
@@ -64,7 +89,7 @@ $COV run $COVFLAGS ../unfoc.py \
 $COV run $COVFLAGS ../unfoc.py \
     --infile $WAIS/orig/xlob/MBL/MKB2l/Y76a/RADnh3/bxds \
     --outdir $OUTDIR/MBL/MKB2l/Y76a/RADnh3 \
-    --channel_def 2  --output_samples 3200 --output_phases \
+    --channel_def 2  --output_samples 3200 --output_phases --bandpass \
     --StackDepth 10 --IncoDepth 5  --Scale 20000  --blanking 200 $UNFOCFLAGS
 
 
@@ -72,7 +97,7 @@ $COV run $COVFLAGS ../unfoc.py \
 $COV run $COVFLAGS ../unfoc.py \
     --infile $WAIS/orig/xlob/ASB1/GCX0e/R40a/RADnh5/bxds \
     --outdir $OUTDIR/ASB1/GCX0e/R40a/RADnh5 \
-    --channel_def ${HICARS2_CHANNELS}  --output_samples 3200 --output_phases \
+    --channel_def ${HICARS2_CHANNELS}  --output_samples 3200 --output_phases --bandpass \
     --StackDepth 10 --IncoDepth 5  --Scale 20000  --blanking 200 $UNFOCFLAGS
 
 
@@ -80,9 +105,11 @@ $COV run $COVFLAGS ../unfoc.py \
 $COV run $COVFLAGS ../unfoc.py \
     --infile $WAIS/orig/xlob/ASB1/JKB2s/R06a/RADnh5/bxds \
     --outdir $OUTDIR/ASB1/JKB2s/R06a/RADnh5 \
-    --channel_def  ${MARFA_CHANNELS}   --output_samples 3200 --output_phases \
-    --StackDepth 10 --IncoDepth 5  --Scale 20000  --blanking 200 $UNFOCFLAGS
+    --channel_def  ${MARFA_CHANNELS}   --output_samples 3200 --output_phases --bandpass \
+    --StackDepth 10 --IncoDepth 5  --Scale 20000  --blanking 200
 
+# Checksum all output data
+find $OUTDIR -type f -print0 | sort -z | xargs -r0 sha1sum -b > ${OUTDIR}.sha1
 
 
 
@@ -91,4 +118,4 @@ $COV run $COVFLAGS ../unfoc.py \
 #    ./$x
 #end
 
-echo "${S0}: Run '$COV combine && $COV report' to view coverage"
+echo "${S0}: Run '$COV report -m' to view coverage"
