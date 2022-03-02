37d5b1956c5a9611bdf437e231c1f66ca9bfe883
WAIS <wais@freeze.ig.utexas.edu>
2022-03-02 17:10:09 -0600

    Add random access memmap class. GNG

--- radbxds.py
+++ radbxds.py
@@ -85,7 +85,7 @@ def index_RADnhx_bxds(input_filename, stream=None):
             input_samples = header.nsamp
             assert 0 < input_samples < 10000
 
-            yield (fpos, headerlen, header.choff) #, ctdata)
+            yield fpos, headerlen, header.choff, header.nsamp #, ctdata)
             # Skip the rest of this record without yielding any values
             fd.seek(4*input_samples, os.SEEK_CUR)
 
@@ -177,6 +177,64 @@ def read_RADnhx_gen(bxds_filename, channel, stream=None):
         fd.close()
 
 
+class BxdsMemmap:
+    """ Reader for random access to traces in a RADnh3 or RADnh5 bxds as numpy arrays """
+    def __init__(self, filename, channel, stream=None):
+        """ Initialize the reader to access one channel's records """
+        self.filename = filename
+        self.index = None
+        self.mmbxds = None
+        self.fd = None
+        self.channel = channel
+        self.choff = 2*(self.channel & 0x01)
+        self.fd = open(filename, 'rb')
+        self.mmbxds = mmap.mmap(self.fd.fileno(), 0, access=mmap.ACCESS_READ)
+        self.load_index(stream)
+
+    def __del__(self):
+        if self.mmbxds:
+            self.mmbxds.close()
+        if self.fd:
+            self.fd.close()
+
+    def load_index(self, stream=None):
+        """ Load record index for bxds 
+        This ignores sequence numbers and assumes that there are no
+        out-of-order radar records within a channel.
+        In theory we could sort the records by sequence number.
+        """
+        self.index = []
+        choff = self.channel - self.channel & 1
+        for item in index_RADnhx_bxds_mmap(self.filename, stream=stream):
+            if item[2] == choff:
+                self.index.append(item)
+
+    def __getitem__(self, idx):
+        """ Return a trace.
+        Example:
+        bxdsmm = BxdsMemmap(bxdsfile, channel=2)
+        # Get the 6th trace for channel 5
+        trace = bxdsmm[5]
+         """
+        if idx > len(self.index):
+            return None
+        fpos, headerlen, rchoff, nsamp = self.index[idx]
+        # offset in bytes for even vs odd channels
+        i0 = fpos + headerlen + self.choff * nsamp
+        trace1 = np.frombuffer(self.mmbxds, dtype="<i2", offset=i0, count=nsamp)
+        return trace1 #yield unfoc.Trace(self.channel, trace1, ctinfo)
+
+def test_class(bxdsfile=None):
+    if bxdsfile is None:
+        bxdsfile = '/disk/kea/WAIS/orig/xlob/NIS4/IBH0e/X84b/RADnh5/bxds'
+    for channel in (0, 1, 2, 3):
+        print("Checking channel", channel)
+        rread = BxdsMemmap(bxdsfile, channel=channel, stream='RADnh5')
+        print(len(rread.index), " records")
+        for n, idx in enumerate(rread.index):
+            s = rread[n].shape
+            if n == 0:
+                print(s)
 
 def main():
     # type: () -> None
@@ -212,6 +270,8 @@ def main():
         pass
 
     logging.info("%d records", ii+1)
+    test_class(args.infile)
+
     elapsed_time = time.time() - t0
     logging.info("Elapsed time: %0.1f sec", elapsed_time)
 
