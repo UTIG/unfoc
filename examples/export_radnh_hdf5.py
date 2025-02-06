8af879287db19963d3730223e9576cee94f08425
WAIS <wais@freeze.ig.utexas.edu>
2025-02-06 15:38:45 -0600

    Change layout and add argument parsing for command line interface. GNG

--- examples/export_radnh_hdf5.py
+++ examples/export_radnh_hdf5.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3
 
-""" Read a RADnh3 or RADnh5 bxds file, and export
-binary files for each channel's radar traces and metadata.
+""" Read a RADnh3 or RADnh5 bxds file, and export an
+HDF5 file for each channel's radar traces and metadata.
+
+Open issues
+- naming of datasets
+  * expose digitizers? yes?
+- chunk size for read performance
+  * consider case where we read a horizontal slow time slice
+- change writing since we only write one way
 """
 
 import os
 import sys
 from pathlib import Path
-import struct
 import datetime
 import logging
+import argparse
 
 import numpy as np
 import h5py
@@ -17,56 +24,66 @@ import h5py
 sys.path.insert(1, '../src')
 import unfoc
 
+COMPRESSION_KWARGS = {'compression': 'gzip', 'compression_opts': 9}
+
+
 def main():
-    WAIS = os.getenv('WAIS')
+    #WAIS = os.getenv('WAIS')
     #bxdsfile = Path(WAIS) / 'orig/xlob/TAM/MKB2o/BDMG01a/RADnh5/bxds'
-    bxdsfile = Path(WAIS) / 'orig/xlob/THW2/UBH0c/Y34b/RADnh5/bxds'
-    outfile = Path('out1') / 'radar.h5'
+    #bxdsfile = Path(WAIS) / 'orig/xlob/THW2/UBH0c/Y34b/RADnh5/bxds'
+    #outfile = Path('out1') / 'radar.h5'
 
-    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+    parser = argparse.ArgumentParser(description='Convert RADnh3/RADnh5 binary data to HDF5 format')
+    parser.add_argument('-i', '--input', required=True,
+                        help='Input bxds file with RADnh3/RADnh5 data. Expects sidecar ct or ct.gz file in same directory')
+    parser.add_argument('-o', '--output', required=True,
+                        help='Output HDF5 file')
 
-    export1(bxdsfile, outfile)
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        help='Verbose script output')
 
+    args = parser.parse_args()
 
-def export1(bxdsfile, outfile):
-    """ A simple export scheme that duplicates some data and
-    throws away some internal timing metadata """
-    #radsnm, channels = unfoc.get_radar_type(bxdsfile)
+    loglevel = 'DEBUG' if args.verbose else 'INFO'
+    logging.basicConfig(stream=sys.stdout, level=loglevel)
 
+    export_hdf5(args.input, args.output)
 
+def export_hdf5(bxdsfile, outfile):
     # Determine file layout
     idx_summary = unfoc.radar_index_summary(bxdsfile)
-    print(idx_summary)
+    logging.debug("%r", idx_summary)
+    rformat = idx_summary['format']
 
     Path(outfile).parent.mkdir(exist_ok=True, parents=True)
     with h5py.File(outfile, 'w') as f:
-        # Allocate radargram
-        # Allocate datasets for ct and ELSA metadata
-
         # Write attributes for PST
         write_attributes(f, idx_summary)
 
-        radar_index = write_metadata_radnh5(f, bxdsfile, idx_summary)
-
-        write_radar(f, bxdsfile, radar_index)
-
+        radar_index = write_metadata(f, bxdsfile, idx_summary, rformat=rformat)
+        write_radar(f, bxdsfile, radar_index, rformat=rformat)
 
 
 
-def write_radar(f, bxdsfile:Path, radar_index, chunksize:int=1000, limit:int=None):
+def write_radar(f, bxdsfile:Path, radar_index, rformat:str, chunksize:int=1000, limit:int=None):
+    """ Write per-trace data including samples and stack timing statistics """
     logging.debug("write_radar from %s", str(bxdsfile))
     shape = (f.attrs['number_of_traces'],
              f.attrs['number_of_digitizers'],
              f.attrs['channels_per_digitizer'],
              f.attrs['samples_per_trace'])
-    f.create_dataset('traces', shape=shape,
+
+    f.create_dataset('rad/traces', shape=shape,
                      dtype='>i2',
                      chunks=(chunksize, shape[1], shape[2], shape[3]),
                      # Compress with gzip, but don't try that hard
-                     #compression='gzip', compression_opts=1,
+                     **COMPRESSION_KWARGS
                      )
 
-    f['traces'].description = 'Radar digitizer sample data'
+    desc = 'Real-valued digitizer voltage data. First axis is slow time, ' \
+           'second axis is digitizer, third axis is channel within digitizer, ' \
+           'fourth axis is fast time.'
+    f['rad/traces'].attrs['description'] = desc
 
     # Rtime is statistics about an array of timestamps of each constituent record in this stack,
     # interval mean and std deviation
@@ -75,13 +92,13 @@ def write_radar(f, bxdsfile:Path, radar_index, chunksize:int=1000, limit:int=Non
     metadata_rtime = np.zeros(shape=shape[0:2] + (4,), dtype='>f')
 
 
-    write_chunks = f['traces'].iter_chunks()
+    write_chunks = f['rad/traces'].iter_chunks()
     #for s in write_chunks:
     nsamples = shape[-1]
     with open(bxdsfile, 'rb') as fhbxds:
         for chunknum, s in enumerate(write_chunks):
             logging.debug("chunk %d %r", chunknum, s)
-            chunk = np.empty(f['traces'][s].shape, dtype='>i2')
+            chunk = np.empty(f['rad/traces'][s].shape, dtype='>i2')
 
             for ii, tracenum in enumerate(range(s[0].start, s[0].stop, s[0].step)):
                 for jj, digitizernum in enumerate(range(s[1].start, s[1].stop, s[1].step)):
@@ -92,39 +109,43 @@ def write_radar(f, bxdsfile:Path, radar_index, chunksize:int=1000, limit:int=Non
                         metadata_rtime[tracenum, digitizernum, :] = 0.
                         continue
 
-                    fhbxds.seek(int(fpos) - 4*8) # back up so we can get the rtimes
-                    rtimes = np.fromfile(fhbxds, dtype='>d', count=4)
-                    metadata_rtime[tracenum, digitizernum, :] = rtimes
+                    if rformat == 'RADnh5':
+                        fhbxds.seek(int(fpos) - 4*8) # back up so we can get the rtimes
+                        rtimes = np.fromfile(fhbxds, dtype='>d', count=4)
+                        metadata_rtime[tracenum, digitizernum, :] = rtimes
+                    else:
+                        fhbxds.seek(int(fpos)) # back up so we can get the rtimes
+
                     traces = np.fromfile(fhbxds, dtype='>i2', count=2*nsamples).reshape((2, nsamples))
                     chunk[ii, jj, :, :] = traces
 
-            f['traces'][s] = chunk
+            f['rad/traces'][s] = chunk
 
             if limit is not None and chunknum >= limit:
                 break
 
-    # Write the rtimes arrays
-    f.create_dataset('stack_timing/std_dev', data=metadata_rtime[:, :, 0:2], compression='gzip')
-    f['stack_timing/std_dev'].attrs['description'] = 'Standard deviation of interval between timestamps of traces used stack, in seconds'
-    f.create_dataset('stack_timing/mean', data=metadata_rtime[:, :, 2:4], compression='gzip')
-    f['stack_timing/mean'].attrs['description'] = 'Mean of interval between timestamps of traces used in stack, in seconds'
+    if rformat == 'RADnh5':
+        # Write the rtimes arrays
+        f.create_dataset('rad/stack_timing/std_dev', data=metadata_rtime[:, :, 0:2], **COMPRESSION_KWARGS)
+        f['rad/stack_timing/std_dev'].attrs['description'] = 'Standard deviation of interval between timestamps of traces used in onboard stack, in seconds'
+        f.create_dataset('rad/stack_timing/mean', data=metadata_rtime[:, :, 2:4], **COMPRESSION_KWARGS)
+        f['rad/stack_timing/mean'].attrs['description'] = 'Mean of interval between timestamps of traces used in onboard stack, in seconds'
 
     # Write the validity array
     traces_valid = np.zeros(shape[0], dtype='u1')
     for digitizernum in range(shape[1]):
         traces_valid += radar_index['valid'][:, digitizernum] << digitizernum
-    f.create_dataset('traces_valid', data=traces_valid, compression='gzip')
-    f['traces_valid'].attrs['description'] = \
+    f.create_dataset('rad/traces_valid', data=traces_valid, **COMPRESSION_KWARGS)
+    f['rad/traces_valid'].attrs['description'] = \
         'Each element is a bit vector indicating digitizer data are valid. ' \
         'The least significant bit corresponds to the first digitizer. ' \
         'A 1 bit in the position indicates that this digitizer record is valid.'
 
 
 
-
-def write_metadata_radnh5(f, bxdsfile:Path, radar_idx_summary):
+def write_metadata(f, bxdsfile:Path, radar_idx_summary, rformat:str):
     """ Write the metadata contained in bxdsfile to the hdf5 file handle """
-    logging.debug("write_metadata_radnh5")
+    logging.debug("write_metadata(rformat=%s)", rformat)
     gen1 = unfoc.index_RADnhx_bxds(bxdsfile, full_header=True)
     gen2 = unfoc.gen_ct(str(bxdsfile), full=True)
     # Allocate metadata arrays
@@ -158,12 +179,9 @@ In a six channel system:
 [4] low gain center, [5] high gain center
 
 To maintain backward compatibility with previous RADnh3 data, the value 0xff in
-the choff field indicates that exactly two channels (generally on one digitizer
-card) are being digitized by this acquisition system. I have verified that the
-current acquisition code will set this field appropriately any time this is true.
+the choff field indicates that exactly two channels are being digitized.
 """
 
-    # TODO: 2-tuple at beginning
     radnh5_dtype = [
         (('Number of samples per trace', 'nsamp'), 'u2'),
         (('Number of channels in this digitizer record', 'nchan'), 'u1'),
@@ -182,6 +200,11 @@ current acquisition code will set this field appropriately any time this is true
         (('Timestamp count (s)', 'tscount'), 'u4'),
     ]
 
+    # Radnh3 has the same definitions but lacks some extended metadata
+    radnh3_dtype = radnh5_dtype[0:7]
+
+    radnh_dtype = radnh3_dtype if rformat == 'RADnh3' else radnh5_dtype
+
     ct_dtype = [
         (('ELSA sequence number', 'elsa__seq'), 'u4'),
         (('ELSA clk year', 'elsa__clk__year'), 'u2'),
@@ -199,38 +222,52 @@ current acquisition code will set this field appropriately any time this is true
         ('valid', 'u1'), # Is this entry valid?
     ]
 
-    metadata = np.zeros(shape=myshape, dtype=radnh5_dtype)
+    metadata = np.zeros(shape=myshape, dtype=radnh_dtype)
     metadata_ct = np.zeros(shape=myshape, dtype=ct_dtype)
     radar_index = np.zeros(shape=myshape, dtype=radar_index_dtype)
     psts = set()
 
-    logging.debug("write_metadata_radnh5: iterating over metadata")
+    logging.debug("write_metadata: iterating over metadata")
     rseq0, rseq1 = radar_idx_summary['rseq_valid_range']
     for ii, ((fpos, headerlen, header), ctrec) in enumerate(zip(gen1, gen2)):
-        if not (rseq0 <= header.rseq <= rseq1):
-            continue
-        assert header.scount == 32, "This code will break if stack count != 32"
-        assert header.tscount == 4, "The rtimes code will break if this is not 4"
-        idx0 = (header.rseq - rseq0) // 32
-        idx1 = header.choff // 2
+
+        if rformat == 'RADnh3':
+            rseq = ctrec[3]
+            if not (rseq0 <= rseq <= rseq1):
+                continue
+
+            idx0 = rseq - rseq0
+        else: # RADnh5
+            if not (rseq0 <= header.rseq <= rseq1):
+                continue
+            assert header.scount == 32, "This code will break if stack count != 32"
+            assert header.tscount == 4, "The rtimes code will break if this is not 4"
+            idx0 = (header.rseq - rseq0) // 32
+
+        choff = 0 if header.choff == 0xff else header.choff
+        idx1 = choff >> 1
         metadata[idx0, idx1] = header
 
+        psts.add('/'.join(ctrec[0:3]))
         metadata_ct[idx0, idx1] = ctrec[3:]
 
         radar_index[idx0, idx1] = (fpos + headerlen, 1)
 
+    # Usually there is exactly 1, but just in case that is not true.
+    f.attrs['pst'] = ','.join(sorted(psts))
+
     for (title, name), _ in ct_dtype:
         name1 = name.replace('__', '/') # put in a subgroup
         logging.debug("writing %s (%s)", name1, title)
-        f.create_dataset(name1, data=metadata_ct[name], compression='gzip')
+        f.create_dataset(name1, data=metadata_ct[name], **COMPRESSION_KWARGS)
         f[name1].attrs['description'] = title
 
 
-    for (title, name), _ in radnh5_dtype:
+    for (title, name), _ in radnh_dtype:
         if name == 'resvd2':
             continue
         logging.debug("writing %s (%s)", name, title)
-        f.create_dataset(name, data=metadata[name], compression='gzip')
+        f.create_dataset(name, data=metadata[name], **COMPRESSION_KWARGS)
         f[name].attrs['description'] = title
 
     return radar_index
@@ -239,9 +276,10 @@ current acquisition code will set this field appropriately any time this is true
 
 def write_attributes(f, idx_summary):
     """ write the attributes to the hdf5 file root dataset """
-    f.attrs['stream_name'] = idx_summary['format']
-    f.attrs['source_filesize'] = idx_summary['filesize']
-    f.attrs['source_modified_time'] = datetime.datetime.fromtimestamp(idx_summary['filemtime']).strftime("%c")
+    f.attrs['format'] = idx_summary['format']
+    f.attrs['source_file_size'] = idx_summary['filesize']
+    mtime = datetime.datetime.fromtimestamp(idx_summary['filemtime']).strftime("%Y-%m-%dT%H:%M:%S")
+    f.attrs['source_modified_time'] = mtime
     f.attrs['number_of_traces'] = idx_summary['valid_records']
     f.attrs['number_of_digitizers'] = idx_summary['nchan']
     f.attrs['channels_per_digitizer'] = 2
